22f29487e0998f36efbc2786441cd09a7286eca7
Riley Martin <riley.willow@gmail.com>
2021-08-24 16:54:02 -0600

    Write functions for cranky centaur

--- mythical-creatures/exercises/centaur.js
+++ mythical-creatures/exercises/centaur.js
@@ -1,6 +1,35 @@
 class Centaur {
-  constructor({name: name}) {
+  constructor({name: name, type: type}) {
     this.name = name
+    this.breed = type
+    this.cranky = false
+    this.standing = true
+    this.actionCount = 0
+  }
+
+  action() {
+    this.actionCount++
+    if (this.actionCount >= 3) {
+      this.cranky = true
+    }
+  }
+
+  shootBow() {
+    this.action()
+    if (this.cranky) {
+      return 'NO!'
+    } else {
+      return 'Twang!!!'
+    }
+  }
+
+  run() {
+    this.action()
+    if (this.cranky) {
+      return 'NO!'
+    } else {
+      return 'Clop clop clop clop!!!'
+    }
   }
 }
 
