cb11aaf736a4a1aafd48fe9a45de859bf3d3036c
Riley Martin <riley.willow@gmail.com>
2021-08-24 17:00:06 -0600

    Finish centaur code to pass all tests

--- mythical-creatures/exercises/centaur.js
+++ mythical-creatures/exercises/centaur.js
@@ -4,6 +4,7 @@ class Centaur {
     this.breed = type
     this.cranky = false
     this.standing = true
+    this.layingDown = false
     this.actionCount = 0
   }
 
@@ -16,7 +17,7 @@ class Centaur {
 
   shootBow() {
     this.action()
-    if (this.cranky) {
+    if (this.cranky || this.layingDown) {
       return 'NO!'
     } else {
       return 'Twang!!!'
@@ -25,11 +26,40 @@ class Centaur {
 
   run() {
     this.action()
-    if (this.cranky) {
+    if (this.cranky || this.layingDown) {
       return 'NO!'
     } else {
       return 'Clop clop clop clop!!!'
     }
+  }
+
+  drinkPotion() {
+    if (this.standing) {
+      this.cranky = false
+      this.actionCount = 0
+    } else {
+      return 'Not while I\'m laying down!'
+    }
+  }
+
+  sleep() {
+    if (this.standing) {
+      return 'NO!'
+    } else {
+      this.cranky = false
+      this.actionCount = 0
+      return 'ZZZZ'
+    }
+  }
+
+  layDown() {
+    this.standing = false
+    this.layingDown = true
+  }
+
+  standUp() {
+    this.standing = true
+    this.layingDown = false
   }
 }
 
