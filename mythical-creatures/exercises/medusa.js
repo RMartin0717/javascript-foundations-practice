9ff8286a3e19b316e536d362ed78d58f6610d063
Riley Martin <riley.willow@gmail.com>
2021-08-26 17:39:45 -0600

    Write Medusa file to pass all tests

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,7 +1,21 @@
+var Person = require('../exercises/person');
+var Statue = require('../exercises/statue');
+
 class Medusa {
   constructor(name) {
     this.name = name
     this.statues = []
+  }
+
+  gazeAtVictim(person) {
+    var newStatue = new Statue(person.name)
+    this.statues.push(newStatue)
+    if (this.statues.length > 3) {
+      var freePerson = new Person(this.statues[0].name)
+      freePerson.mood = 'relieved'
+      this.statues.shift()
+      return freePerson
+    }
   }
 }
 
