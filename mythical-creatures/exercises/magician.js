074e24518df588bb8d4489ccc94e307a5fe3dee8
Riley Martin <riley.willow@gmail.com>
2021-09-10 20:18:11 -0600

    Set up constructor

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -1,6 +1,8 @@
 class Magician {
-  constructor() {
-    
+  constructor({ name: name, assistant: assistant, clothing: clothing }) {
+    this.name = `The Great ${name}`
+    this.assistant = assistant
+    this.favoriteAccessory = clothing || 'top hat'
   }
 }
 
