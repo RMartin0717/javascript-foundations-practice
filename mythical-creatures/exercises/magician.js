29a2a3e12a121e06819f91a1fefbc457698fe730
Riley Martin <riley.willow@gmail.com>
2021-09-10 20:25:13 -0600

    Implement confidencePercentage

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -3,13 +3,16 @@ class Magician {
     this.name = `The Great ${name}`
     this.assistant = assistant
     this.favoriteAccessory = clothing || 'top hat'
+    this.confidencePercentage = 10
   }
 
   performIncantation(spell) {
+    this.confidencePercentage += 10
     return `${spell.toUpperCase()}!`
   }
 
   performTrick() {
+    this.confidencePercentage += 10
     if (this.favoriteAccessory === 'top hat') {
       return 'PULL RABBIT FROM TOP HAT'
     } else {
