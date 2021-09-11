e198e7e987ce5e798f24a7a74236e2ea854f3e3d
Riley Martin <riley.willow@gmail.com>
2021-09-11 09:48:38 -0600

    Write completeTransformation method

--- mythical-creatures/exercises/werewolf.js
+++ mythical-creatures/exercises/werewolf.js
@@ -3,6 +3,16 @@ class Werewolf {
     this.name = name
     this.form = 'human'
   }
+
+  completeTransformation() {
+    if (this.form === 'human') {
+      this.form = 'wolf'
+      return 'Aaa-Woooo!'
+    } else {
+      this.form = 'human'
+      return 'Where are I?'
+    }
+  }
 }
 
 module.exports = Werewolf
