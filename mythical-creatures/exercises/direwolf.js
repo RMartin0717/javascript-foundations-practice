db0c85f2831e40dbc5f0cd057a8a2299266e6245
Riley Martin <riley.willow@gmail.com>
2021-09-13 16:41:38 -0600

    Write sayHouseWords method

--- mythical-creatures/exercises/direwolf.js
+++ mythical-creatures/exercises/direwolf.js
@@ -4,13 +4,17 @@ class Direwolf {
    this.home = home || 'Beyond the Wall'
    this.size = size || 'Massive'
    this.starksToProtect = []
+   this.huntsWhiteWalkers = true
  }
 
  protect(stark) {
    if (this.home === stark.location && this.starksToProtect.length < 2) {
      this.starksToProtect.push(stark)
+     stark.safe = true
+     this.huntsWhiteWalkers = false
    }
  }
+
 }
 
 module.exports = Direwolf
--- mythical-creatures/exercises/stark.js
+++ mythical-creatures/exercises/stark.js
@@ -2,6 +2,15 @@ class Stark {
   constructor({ name: name, area: location }) {
     this.name = name
     this.location = location || 'Winterfell'
+    this.safe = false
+  }
+
+  sayHouseWords() {
+    if (this.safe) {
+      return 'The North Remembers'
+    } else {
+      return 'Winter is Coming'
+    }
   }
 }
 
