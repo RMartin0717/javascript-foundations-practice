acffae58a0b39522d1fe33f8bf9b48b05713d13e
Riley Martin <riley.willow@gmail.com>
2021-10-19 12:01:01 -0600

    Write collectRiddle method to pass tests

--- mythical-creatures/exercises/sphinx.js
+++ mythical-creatures/exercises/sphinx.js
@@ -1,6 +1,18 @@
 class Sphinx {
   constructor() {
     this.name = null
+    this.riddles = []
+  }
+
+  collectRiddle(riddle) {
+    this.riddles.push(riddle)
+    if(this.riddles.length === 4) {
+      this.riddles.shift()
+    }
+  }
+
+  attemptAnswer(answer) {
+    if()
   }
 }
 
