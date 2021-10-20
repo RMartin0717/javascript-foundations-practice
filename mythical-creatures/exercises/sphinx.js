5a8cac387cacb1df206247e3b8e88694c0022b9f
Riley Martin <riley.willow@gmail.com>
2021-10-20 13:45:42 -0600

    Pass all Sphinx tests

--- mythical-creatures/exercises/sphinx.js
+++ mythical-creatures/exercises/sphinx.js
@@ -2,6 +2,7 @@ class Sphinx {
   constructor() {
     this.name = null
     this.riddles = []
+    this.heroesEaten = 0
   }
 
   collectRiddle(riddle) {
@@ -15,9 +16,14 @@ class Sphinx {
     for (let i = 0; i < this.riddles.length; i++) {
       if(this.riddles[i].answer === answer) {
         this.riddles.splice(i, 1)
-        return 'That wasn\'t that hard, I bet you don\'t get the next one'
+        if(!this.riddles.length) {
+          return `PSSSSSSS THIS HAS NEVER HAPPENED, HOW DID YOU KNOW THE ANSWER WAS "${answer}"???`
+        } else {
+          return 'That wasn\'t that hard, I bet you don\'t get the next one'
+        }
       }
     }
+    this.heroesEaten++
   }
 }
 
